20064e5d6b6a89587ce516309b4d20379cc125f9
Andreas Wickström Johansson <andreaswjoh@gmail.com>
2020-09-13 21:42:49 +0200

    Add ESLint, with configuration

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        'linebreak-style': 0,
+        // Indent with 4 spaces
+        indent: ['error', 4],
+
+        // Indent JSX with 4 spaces
+        'react/jsx-indent': ['error', 4],
+
+        // Indent props with 4 spaces
+        'react/jsx-indent-props': ['error', 4],
+    },
+};
