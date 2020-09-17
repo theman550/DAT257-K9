bf0e903998d3c6d4519284c59d4575f00c1ff883
ITJohan <johanlindkvist89@gmail.com>
2020-09-17 11:12:47 +0200

    add configurations for eslint, prettier, airbnb, jest and allow .js in lint

--- frontend/src/components/Navigation.test.js
+++ frontend/src/components/Navigation.test.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import {render, screen} from '@testing-library/react'
-import {BrowserRouter as Router} from 'react-router-dom'
-import Navigation from './Navigation'
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from './Navigation';
 
 describe('Navigation', () => {
   beforeEach(() => {
@@ -10,18 +10,18 @@ describe('Navigation', () => {
       <Router>
         <Navigation />
       </Router>
-    )
-  })
+    );
+  });
 
   it('renders Share-a-ride', () => {
-    expect(screen.getByText('Share-a-ride')).toBeInTheDocument()
-  })
+    expect(screen.getByText('Share-a-ride')).toBeInTheDocument();
+  });
 
   it('renders search link', () => {
-    expect(screen.getByText('Search')).toBeInTheDocument()
-  })
+    expect(screen.getByText('Search')).toBeInTheDocument();
+  });
 
   it('renders add trip link', () => {
-    expect(screen.getByText('Add trip')).toBeInTheDocument()
-  })
-})
+    expect(screen.getByText('Add trip')).toBeInTheDocument();
+  });
+});
