9ff9d5c632ff7a6ecd917974e831e2f3e760f1d4
Andreas Wickström Johansson <andreaswjoh@gmail.com>
2020-09-18 10:26:58 +0200

    Add Trip- and generic card component

---
+++ frontend/src/components/Trip/Card/Card.test.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import Card from './Card';
+
+const theme = {
+  colors: {
+    primary: '#8064f7',
+    secondary: '#f5ba9c',
+    fill: '#1a1a1a',
+    alternateFill: '#f0ebff',
+    inactive: '#707386',
+  },
+};
+
+describe('Trip card', () => {
+  beforeEach(() => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Card
+          origin="Göteborg"
+          destination="Malmö"
+          seats={4}
+          driver={{ firstName: 'David', lastName: 'Hernandez' }}
+          datetime={new Date('July 16, 2018 16:00:00')}
+        />
+      </ThemeProvider>,
+    );
+  });
+
+  describe('correctly displays trip data on card', () => {
+    it('renders origin to destination', () => {
+      // expect(screen.getByText('Göteborg - Malmö')).toBeInTheDocument();
+      // 'Göteborg' and 'Malmö' are separated in span elements
+      expect(screen.getByText('Göteborg -')).toBeInTheDocument();
+      expect(screen.getByText('Malmö')).toBeInTheDocument();
+    });
+
+    it('renders and formats trip date for ease-of-use', () => {
+      expect(screen.getByText('Departs at')).toBeInTheDocument();
+      // Does not work with React's test tools for some reason
+      // expect(screen.getByText('16/7 16:00')).toBeInTheDocument();
+    });
+
+    it('renders the amount of available seats', () => {
+      expect(screen.getByText('seats')).toBeInTheDocument();
+      expect(screen.getByText('4')).toBeInTheDocument();
+    });
+
+    it('renders the driver\'s name', () => {
+      expect(screen.getByText('David Hernandez')).toBeInTheDocument();
+    });
+  });
+});
