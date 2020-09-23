c3a32b00d20f0dffe28832897e1cd463b89d6a13
Emil Holmberg <emilholmberg@live.se>
2020-09-23 21:12:47 +0200

    Add files via upload

--- backend/html/api.php
+++ backend/html/api.php
@@ -1,6 +1,11 @@
 <?php
 include("connectDB.php");
-
+//readTrips();
+//echo testTrip(4, false, "test_files/worldcities.csv");
+//filterExpiredTrips("startLocation = 'lidkoping'");
+//echo testTrip(20, "test_files/worldcities.csv");
+//createAccount("flippo1@hotmail.com", "hemligt", "emil", "holmberg");
+//tryLogin("flippo@hotmail.com", "hemligt"); //$2y$10$6EBRayDCHGnIe/zh2Q5oquiQ9GZXGk9EkELS2pHO84ryE7imlHvOy
 #Här tänker jag mig att huvudlogiken för servern ska vara
 if(isset($_GET['function'])){
     switch($_GET['function']){
@@ -31,102 +36,24 @@ function sendResponse($response){
 
 #Om "from" är satt returneras endast resor därifrån, annars alla
 #Den här funktionen går lätt att bygga vidare på med fler argument
-function readTrips(){
-    if(isset($_GET['from'])){ 
-        $sql = "SELECT * FROM Resa WHERE startLocation = '{$_GET['from']}'";
-    }
-    elseif(isset($_GET['tripID'])){
-        $sql = "SELECT * FROM Resa WHERE tripID = '{$_GET['tripID']}'";
-    }
-    elseif(isset($_GET['userID'])){
-        $sql = "SELECT * FROM Resa WHERE userID = '{$_GET['userID']}'";
-    }
-    else{
-        $sql = "SELECT * FROM Resa";
-    }
-    $result = queryDB($sql);
+function readTrips()
+{
+	$filterArray = getTripGETParameters();
+	$query = "SELECT * FROM Resa WHERE '1' = '1'"; // syntax fel i query skapandet om inte '1' = '1' och jag vill lägga till flera ands, om inga argument läggs till returneras alla resor.
+	for($i = 0; $i < count($filterArray); $i++)
+	{
+		if(isset($_GET[$filterArray[$i]]))
+		{
+			$val = $_GET[$filterArray[$i]];
+			$query = $query . " AND $filterArray[$i] = '$val'";
+		}
+	}
+    $result = queryDB($query);
     return $result;
 }
-
-#Skriver en ny resa till databasen
-function writeTrip($startLocation, $destination, $startTime, $price, $seatsAvailable, $description, $userID/*la till variabler temp för testning, ska vara post senare*/){ 
-    $POST = [ #Det här ska simulera den $_POST vi får från frontend
-    'Start' => 'Myskoxe',
-    'Slut' => 'Vildsvin',
-    'Pris' => '321'
-    ];
-    $start = $POST['startLocation'];
-    $slut = $POST['destination'];
-    $pris = $POST['Price'];
-
-    //$sql = "INSERT INTO Resa (Start, Slut, Pris) VALUES ('{$start}', '{$slut}', '{$pris}')";
-	// uppdaterad med nya databas formatet, detta är fortfarande en dummy/exempel insert
-	$sql = "INSERT INTO Resa (startLocation, destination, price, tripID, startTime, seatsAvailable, description, userID) VALUES ('$startLocation', '$destination', '$price', '" . createTripID() . "', '" . date('2020-09-15 22:20:31') . "' , '$seatsAvailable', '$description', '$userID')";
-    queryDB($sql);
-}
-// generar trip-id på form "trip-xxxxx...x"
-function createTripID()
+function getTripGETParameters()
 {
-	return (uniqid('trip-', true));
-}
-// genererar user-id på form "user-xxxx...x"
-function createUserID()
-{
-	return (uniqid('user-', true));
-}
-
-#Printar en query
-function printRows($result){
-    if($result -> num_rows > 0){
-        echo "<b><u>Resultat från query: </br></b></u>";
-        while($row = mysqli_fetch_assoc($result)){
-            echo "<li>" . $row["startLocation"] . " -> " . $row["destination"] . "</br>";
-        }
-    }
-}
-function createAccount($email, $password, $firstname, $lastname)
-{
-	$userID = createUserID();
-	$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-	if(!validMail($email))
-	{
-		// hantera, JSON retur?
-		return null; 
-	}
-	$sql = "INSERT INTO Users (userID, email, firstname, lastname, password) VALUES ('$userID', '$email', '$firstname', '$lastname', '$hashedPassword')";
-	echo $sql;
-	queryDB($sql);
-	return $userID;
-}
-// kollar så att mailen inte redan finns i databas
-function validMail($email)
-{
-	$sql = "SELECT * FROM Users WHERE email = '$email'";
-	$result = queryDB($sql);
-	if (mysqli_num_rows($result) > 0)
-	{
-		return false;
-	}	
-	return true;
-	// if(!filter_var($email, FILTER_VALIDATE_EMAIL)) skulle kunna användas för att kontrollera att formatet av mailen är rätt, men det borde frontend göra tycker jag	
-}
-// returnar lösenordsHash i databas
-function getPassword($email)
-{
-	$query = "SELECT password FROM Users WHERE email ='$email'";
-	$password = queryDB($query);
-	return $password;
-}
-// https://stackoverflow.com/questions/1354999/keep-me-logged-in-the-best-approach länk om hur man lägger till bra säkerhet enkelt.
-// jämnför om lösenord i plaintext angivet för en email matchar den hashade verisionen av det lösenordet
-function tryLogin($email, $password)
-{	
-	$row = mysqli_fetch_assoc(getPassword($email));
-	if(password_verify($password, $row['password']))
-	{
-		// sätt session <-- http://www.learningaboutelectronics.com/How-to-use-sessions-to-track-user-data-using-PHP.php
-		echo 'logged in';
-	}
+	return array("startLocation", "destination", "price", "tripID", "startTime", "seatsAvailable", "description", "userID");
 }
 function tripExists($tripID)
 {
@@ -179,9 +106,93 @@ function testTrip($numOfTests, $onlyAdd = false, $path ="testing/worldcities.csv
 	fclose($handle);
 	return true;
 }
+#Skriver en ny resa till databasen
+function writeTrip($startLocation, $destination, $startTime, $price, $seatsAvailable, $description, $userID/*la till variabler temp för testning, ska vara post senare*/){ 
+    /*$POST = [ #Det här ska simulera den $_POST vi får från frontend
+    'Start' => 'Myskoxe',
+    'Slut' => 'Vildsvin',
+    'Pris' => '321'
+    ];
+    $start = $POST['startLocation'];
+    $slut = $POST['destination'];
+    $pris = $POST['Price'];*/
+
+    //$sql = "INSERT INTO Resa (Start, Slut, Pris) VALUES ('{$start}', '{$slut}', '{$pris}')";
+	// uppdaterad med nya databas formatet, detta är fortfarande en dummy/exempel insert
+	$tripID = createTripID();
+	$sql = "INSERT INTO Resa (startLocation, destination, price, tripID, startTime, seatsAvailable, description, userID) VALUES ('$startLocation', '$destination', '$price', '$tripID', '" . date('2020-09-15 22:20:31') . "' , '$seatsAvailable', '$description', '$userID')";
+    queryDB($sql);
+	return $tripID; // detta är mest för testningens skull, men borde kanske alltid returnera? Ponera: skapar resa, resa ändras direkt efter på frontend, förutsätter att en resa som läggs till ska visas någonstans och att man vill ha en referens i form av id till den resan
+}
+// generar trip-id på form "trip-xxxxx...x"
+function createTripID()
+{
+	return (uniqid('trip-', true));
+}
+// genererar user-id på form "user-xxxx...x"
+function createUserID()
+{
+	return (uniqid('user-', true));
+}
+#Printar en query
+function printRows($result){
+    if($result -> num_rows > 0){
+        echo "<b><u>Resultat från query: </br></b></u>";
+        while($row = mysqli_fetch_assoc($result)){
+            echo "<li>" . $row["startLocation"] . " -> " . $row["destination"] . "</br>";
+        }
+    }
+}
+// skapar nytt konto i databas om mail inte redan finns inlagd, lösenord hashas också innan
+function createAccount($email, $password, $firstname, $lastname)
+{
+	$userID = createUserID();
+	$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+	if(!validMail($email))
+	{
+		// hantera, JSON retur?
+		return null; 
+	}
+	$sql = "INSERT INTO Users (userID, email, firstname, lastname, password) VALUES ('$userID', '$email', '$firstname', '$lastname', '$hashedPassword')";
+	echo $sql;
+	queryDB($sql);
+	return $userID;
+}
+// kollar så att mailen inte redan finns i databas
+function validMail($email)
+{
+	$sql = "SELECT * FROM Users WHERE email = '$email'";
+	$result = queryDB($sql);
+	if (mysqli_num_rows($result) > 0)
+	{
+		return false;
+	}	
+	return true;
+	// if(!filter_var($email, FILTER_VALIDATE_EMAIL)) skulle kunna användas för att kontrollera att formatet av mailen är rätt, men det borde frontend göra tycker jag	
+}
+// returnar lösenordsHash i databas
+function getPassword($email)
+{
+	$query = "SELECT password FROM Users WHERE email ='$email'";
+	$password = queryDB($query);
+	return $password;
+}
+// https://stackoverflow.com/questions/1354999/keep-me-logged-in-the-best-approach länk om hur man lägger till bra säkerhet enkelt.
+// jämnför om lösenord i plaintext angivet för en email matchar den hashade verisionen av det lösenordet
+function tryLogin($email, $password)
+{	
+	$row = mysqli_fetch_assoc(getPassword($email));
+	if(password_verify($password, $row['password']))
+	{
+		// sätt session <-- http://www.learningaboutelectronics.com/How-to-use-sessions-to-track-user-data-using-PHP.php
+		echo 'logged in';
+	}
+}
+// tar bort alla tider som gått ut, användning 0 parametrar ger alla tider som expireats utan filter, med extraconditions satt kan t.ex filterExpiredTrips("startLocation = 'lidkoping'"); som ger alla resor som inte gått ut och startar i lidkoping
 function filterExpiredTrips($extraConditions = null)
 {
 	$currentTime = date('d-m-y h:i:s');
+	echo $currentTime;
 	$query;
 	if($extraConditions != null)
 	{
