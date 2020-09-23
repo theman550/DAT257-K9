4cec133fa43ab4faf926c92f0cf10842e7503175
Andreas Wickström Johansson <andreaswjoh@gmail.com>
2020-09-23 13:55:32 +0200

    Implement theme provider with a global color scheme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import Navigation from './components/Navigation';
+
+/**
+ * Access like this:
+ * const MyFancyButton = styled.button`
+ * background: ${props => props.theme.colors.primary}
+ * `
+ */
+const theme = {
+  colors: {
+    primary: '#8064f7',
+    secondary: '#f5ba9c',
+    fill: '#1a1a1a',
+    alternateFill: '#f0ebff',
+    inactive: '#707386',
+  },
+};
 
 function App() {
   return (
-    <Router>
-      <Navigation />
-      <Switch>
-        <Route path="/search">
-          <p>Search page</p>
-        </Route>
-        <Route path="/add">
-          <p>Add trip page</p>
-        </Route>
-        <Route path="/login">
-          <p>Login page</p>
-        </Route>
-        <Route path="/">
-          <p>Home page</p>
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/search">
+            <p>Search page</p>
+          </Route>
+          <Route path="/add">
+            <p>Add trip page</p>
+          </Route>
+          <Route path="/login">
+            <p>Login page</p>
+          </Route>
+          <Route path="/">
+            <p>Home page</p>
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
