8c8d57ae2033c2d3dae012addd4c358725c0484e
ITJohan <johanlindkvist89@gmail.com>
2020-09-23 16:57:20 +0200

    change design on add trip component (Bashars work)

--- frontend/src/components/AddTrip.js
+++ frontend/src/components/AddTrip.js
@@ -14,12 +14,37 @@ const Form = styled.form`
   padding: 20px;
   text-align: center;
   position: absolute;
-  top: 50%;
+  top: 60%;
   left: 50%;
   transform: translate(-50%, -50%);
   background-size: cover;
   border-radius: 5px;
   font-size: medium;
+`;
+
+const BG = styled.div`
+  background: linear-gradient(to top right, #060628, #1F245A, #682359);
+  position: fixed;
+  top: 2;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Discription = styled(Field)`
+  background-color: #262626;
+  border-radius: 5px;
+  box-shadow: 0 15px 35px rgba(50, 50, 93, 0.1), 0 5px 15px rgba(0, 0, 0, 0.07);
+  color:white;
+  font-size: large;
+`;
+
+const Div = styled.div`
+  width: 30.3%;
+  font-size: 18px;
+  line-height: 1.2;
+  float:left;
+  border-radius: 20px;
 `;
 
 const Input = styled(Field)`
@@ -32,12 +57,44 @@ const Input = styled(Field)`
   line-height: 1.2;
   outline: none;
   color: #9677D9;
+  
   ::-webkit-input-placeholder { /* Chrome/Opera/Safari */
-  color: #9677D9;
-  font-size: medium;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-  opacity: 0.3;
-}
+    color: #9677D9;
+    font-size: medium;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    opacity: 0.3;
+    border-radius: 5px;
+    box-shadow: 0 15px 35px rgba(50, 50, 93, 0.1), 0 5px 15px rgba(0, 0, 0, 0.07);
+  }
+`;
+
+const Seats = styled(Input)`
+  width: 20%;
+  line-height: 1.2;
+`;
+
+const Price = styled(Input)`
+  width: 20%;
+  line-height: 1.2;
+  border-style:ridge;
+  color:white;
+  border-radius: 5px;
+  border-color: #9677D9;
+`;
+
+const Option = styled(Input)`
+  width: 70%;
+  font-size: 18px;
+  line-height: 1.2;
+`;
+
+const Dis = styled(Div)`
+  width: 70%;
+  font-size: 18px;
+  line-height: 1.2;
+  float:left;
+  margin-top:-100px;
+  margin-left:-40px;
 `;
 
 const Label = styled.label`
@@ -48,18 +105,68 @@ const Label = styled.label`
 const Button = styled.button`
   position: relative;
   margin: 15px 0 5px 0;
-  margin-top: 0px;
-  left: 30%;
+  margin-top: 20px;
+  left: 10%;
   transform: translateX(-50%);
-  background: #9677D9;
   border: none;
   border-radius: 5px;
   padding: 10px;
   color: white;
-  width: 60%;
+  width: 30%;
   font-family: "Noto Sans", sans-serif;
   font-size: 14px;
   cursor: pointer;
+  background-color: transparent;
+  border: 1px solid #d0d0d0;
+  color: #B8B8B8;
+    
+  &:before,
+  &:after {
+    content: '';
+    border-style: solid;
+    position: absolute;
+    z-index: 5;
+    border-radius: 3px;
+    box-sizing: content-box;
+    @extend .animate;
+  }
+  
+  &:before {
+    width: 0;
+    height: 100%;
+    border-width: 1px 0 1px 0;
+    top: -1px;
+    left: 0;
+    transition-delay: 0.05s;
+  }
+  
+  &:after {
+    width: 100%;
+    height: 0;
+    border-width: 0 1px 0 1px;
+    top: 0;
+    left: -1px;
+  }
+  
+  &:hover {
+    &:before {
+      width: 100%;
+    }
+    
+    &:after {
+      height: 100%;
+    }
+  }
+  
+  &:before,
+  &:after {
+    border-color: #9677D9;
+  }
+  
+  &:hover {
+    border-color: #9677D9;
+    color: White;
+  }
 `;
 
 const Icon = styled(MapPin)`
@@ -69,51 +176,74 @@ const Icon = styled(MapPin)`
   right: 20px;
 `;
 
-const onSubmit = (values) => {
-  console.log(values); // eslint-disable-line
+const onSubmit = () => {
+  // console.log(JSON.stringify(values));
 };
 
 const form = (props) => (
-  <div className="Addtrip">
+  <div>
+
+    <BG />
     <Form onSubmit={props.handleSubmit} className="Form">
       <Icon size={17} className="icon" />
-      <Input name="origin" placeholder="Origin" className="input" />
+      <Input name="startLocation" placeholder="Origin" className="input" />
       <Icon size={17} className="icon" />
-      <Input name="destenation" placeholder="Destenation" className="input" />
-      <div className="seats">
+      <Input name="destination" placeholder="Destenation" className="input" />
+      <Div className="seats">
         <Label>Seats</Label>
         <br />
-        <Input name="seats" component="select" className="seats">
+        <br />
+        <Seats name="seatsAvailable" component="select" className="seats">
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
           <option value="4">4</option>
-        </Input>
-      </div>
+        </Seats>
+      </Div>
 
-      <div>
+      <Div>
         <Label>Date</Label>
         <br />
-        <Input name="date" type="date" className="input" />
-      </div>
-      <div>
+        <br />
+        <Option name="startTime" type="date" />
+      </Div>
+      <Div>
         <Label>Time</Label>
         <br />
-        <Input name="time" type="time" className="input" />
-      </div>
+        <br />
+        <Option name="time" type="time" />
+      </Div>
+
+      <Div>
+        <Label>Price</Label>
+        <br />
+        <br />
+        <Price name="price" />
+        <Label> SEK </Label>
+      </Div>
+
+      <Dis>
+        <Label>Description</Label>
+        <br />
+        <br />
+
+        <Discription name="description" component="textarea" rows="7" cols="50" />
+
+      </Dis>
+
       <i className="fal fa-bus-alt" />
       <Button data-testid="add-button" type="submit" className="Modal">ADD Ride</Button>
-
     </Form>
-  </div>
-);
 
+  </div>
+
+);
 const AddTrip = () => (
   <div className="SearchGui">
     <Formik
       data-testid="form"
       initialValues={{
-        origin: '', destenation: '', seats: '1', date: '', time: '',
+        startLocation: '', destination: '', seatsAvailable: '1', startTime: '', time: '', price: '', description: '',
       }}
       onSubmit={onSubmit}
     >
