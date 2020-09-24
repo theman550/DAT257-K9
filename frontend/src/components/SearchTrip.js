5028723a1fc5e8345cbf06e8c586716ed32b7af9
ITJohan <johanlindkvist89@gmail.com>
2020-09-24 11:14:09 +0200

    add modal feature to search trips

--- frontend/src/components/SearchTrip.js
+++ frontend/src/components/SearchTrip.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
+import {X} from 'react-feather'
 import {
   FieldFactory,
   PrimaryButton,
@@ -8,17 +9,66 @@ import {
 
 const StyledInput = FieldFactory(styled.input``)
 
+const CloseButton = styled(X)`
+
+`
+
+const Modal = styled.div`
+  /*display: none;*/
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+  -webkit-animation-name: fadeIn; /* Fade in the background */
+  -webkit-animation-duration: 0.4s;
+  animation-name: fadeIn;
+  animation-duration: 0.4s
+
+  @-webkit-keyframes slideIn {
+    from {bottom: -300px; opacity: 0} 
+    to {bottom: 0; opacity: 1}
+  }
+
+  @keyframes slideIn {
+    from {bottom: -300px; opacity: 0}
+    to {bottom: 0; opacity: 1}
+  }
+
+  @-webkit-keyframes fadeIn {
+    from {opacity: 0} 
+    to {opacity: 1}
+  }
+
+  @keyframes fadeIn {
+    from {opacity: 0} 
+    to {opacity: 1}
+  }
+`
+
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   background-color: ${props => props.theme.colors.fill};
-  max-width: 400px;
   padding: 1rem;
-  margin: 1rem;
+  margin: 30% auto;
+  width: 80%;
   border-radius: 10px;
   -webkit-box-shadow: -10px 10px 40px 0px rgba(10,10,10,0.75);
   -moz-box-shadow: -10px 10px 40px 0px rgba(10,10,10,0.75);
   box-shadow: -10px 10px 40px 0px rgba(10,10,10,0.75);
+
+  /* modal functionality */
+  position: fixed;
+  bottom: 0;
+  -webkit-animation-name: slideIn;
+  -webkit-animation-duration: 0.4s;
+  animation-name: slideIn;
+  animation-duration: 0.4s
 `
 
 const StyledTextRow = styled.div`
@@ -63,44 +113,53 @@ const StyledButton = styled(PrimaryButton)`
 `
 
 const SearchTrip = () => {
+  const [showModal, setShowModal] = useState(false);
+
   const handleSubmit = e => {
     e.preventDefault();
+
+    console.log('test')
   }
 
   return (
-    <StyledForm aria-label='Search form' onSubmit={handleSubmit}>
-      <StyledTextRow>
-        <Label htmlFor='from'>From</Label>
-        <StyledInput type='text' id='from' placeholder='Enter start location...'></StyledInput>
-      </StyledTextRow>
-      <StyledTextRow>
-        <Label htmlFor='to'>To</Label>
-        <StyledInput type='text' id='to' placeholder='Enter destination...'></StyledInput>
-      </StyledTextRow>
-      <StyledSelectRow>
-        <StyledSelectColumn>
-          <Label htmlFor='date'>Date</Label>
-          <StyledInput type='date' id='date'></StyledInput>
-        </StyledSelectColumn>
-        <StyledSelectColumn>
-          <Label htmlFor='time'>Time</Label>
-          <StyledInput type='time' id='time'></StyledInput>
-        </StyledSelectColumn> 
-      </StyledSelectRow>
-      <StyledSelectRow>
-        <StyledSelectColumn>
-          <Label htmlFor='seats'>Seats</Label>
-          <StyledInput type='number' id='seats' min='0' max='4'></StyledInput>
-        </StyledSelectColumn>
-        <StyledSelectColumn>
-          <Label htmlFor='price'>Price</Label>
-          <StyledInput type='number' id='price' min='0' max='1000'></StyledInput>
-        </StyledSelectColumn>
-      </StyledSelectRow>
-      <StyledSelectRow>
-        <StyledButton type='submit'>Search</StyledButton>
-      </StyledSelectRow>
-    </StyledForm>
+    <Modal>
+      <StyledForm aria-label='Search form' onSubmit={handleSubmit}>
+        <StyledTextRow>
+          <CloseButton onClick={() => setShowModal(false)} />
+        </StyledTextRow>
+        <StyledTextRow>
+          <Label htmlFor='from'>From</Label>
+          <StyledInput type='text' id='from' placeholder='Enter start location...'></StyledInput>
+        </StyledTextRow>
+        <StyledTextRow>
+          <Label htmlFor='to'>To</Label>
+          <StyledInput type='text' id='to' placeholder='Enter destination...'></StyledInput>
+        </StyledTextRow>
+        <StyledSelectRow>
+          <StyledSelectColumn>
+            <Label htmlFor='date'>Date</Label>
+            <StyledInput type='date' id='date'></StyledInput>
+          </StyledSelectColumn>
+          <StyledSelectColumn>
+            <Label htmlFor='time'>Time</Label>
+            <StyledInput type='time' id='time'></StyledInput>
+          </StyledSelectColumn> 
+        </StyledSelectRow>
+        <StyledSelectRow>
+          <StyledSelectColumn>
+            <Label htmlFor='seats'>Seats</Label>
+            <StyledInput type='number' id='seats' min='0' max='4'></StyledInput>
+          </StyledSelectColumn>
+          <StyledSelectColumn>
+            <Label htmlFor='price'>Price</Label>
+            <StyledInput type='number' id='price' min='0' max='1000'></StyledInput>
+          </StyledSelectColumn>
+        </StyledSelectRow>
+        <StyledSelectRow>
+          <StyledButton type='submit'>Search</StyledButton>
+        </StyledSelectRow>
+      </StyledForm>
+    </Modal>
   )
 }
 
