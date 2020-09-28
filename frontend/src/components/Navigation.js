fd7ce438be124119f92e8e015581094f513eedd7
ITJohan <johanlindkvist89@gmail.com>
2020-09-28 16:50:13 +0200

    make navbar somewhat responsive

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { LogIn, Map } from 'react-feather';
+import { H2 } from '../components/UI';
 
 const Nav = styled.nav`
   display: flex;
@@ -11,7 +12,23 @@ const Nav = styled.nav`
   width: 100%;
   background-color: rgba(0, 0, 0, 0.7);
   padding: ${props => props.theme.padding.section};
+
+  @media only screen and (min-width: 765px) {
+    & > h2 {
+      flex-grow: 20;
+    }
+       
+    & > a {
+      flex-grow: 1;
+    }
+  }
 `;
+
+const StyledH2 = styled(H2)`
+  @media only screen and (max-width: 765px) {
+    display: none;
+  }
+`
 
 const StyledLogInIcon = styled(LogIn)`
   color: ${props => props.theme.colors.primary};
@@ -32,6 +49,7 @@ const StyledMapIcon = styled(Map)`
 const Navigation = () => {
   return (
     <Nav>
+      <StyledH2>Share-a-ride</StyledH2> 
       <Link aria-label='Account' to="/account"><StyledLogInIcon /></Link>
       <Link aria-label='Trips' to="/"><StyledMapIcon /></Link>
     </Nav>
