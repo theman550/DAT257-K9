72eb479cbe7f41e0fba44bc2b7696a8c01cad1b5
KaramAghi <aghi.karam@gmail.com>
2020-09-28 22:23:45 +0200

    Responsive login with all funcktion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,40 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Navigation from './components/Navigation';
+import Trips from './screens/Trips';
+import AddTrip from './components/AddTrip';
+import Notification from './components/Notification';
 import Login from './components/Login';
 
+/**
+ * Access like this:
+ * const MyFancyButton = styled.button`
+ * background: ${props => props.theme.colors.primary}
+ * `
+ */
+const theme = {
+  colors: {
+    primary: '#8064f7',
+    secondary: '#f5ba9c',
+    fill: '#1a1a1a',
+    alternateFill: '#f0ebff',
+    inactive: '#707386',
+  },
+};
+
+const App = () => {
+  const [notification, setNotification] = useState(null);
+
+  // TODO: First to use this function can remove the next line
+  // eslint-disable-next-line
+  const showNotification = (msg, color, seconds) => {
+    setNotification({ msg, color });
+    setTimeout(() => {
+      setNotification(null);
+    }, seconds * 1000);
+  };
+
   return (
-<<<<<<< HEAD
-    <Router>
-      <Navigation />
-      <Switch>
-        <Route path="/search">
-          <p>Search page</p>
-        </Route>
-        <Route path="/add">
-          <p>Add trip page</p>
-        </Route>
-        <Route path="/login">
-          <p>Login page</p>
-          <Login />
-        </Route>
-        <Route path="/">
-          <p>Home page</p>
-        </Route>
-      </Switch>
-    </Router>
-=======
     <ThemeProvider theme={theme}>
       <Router>
         <Navigation />
@@ -43,6 +54,7 @@ import Login from './components/Login';
           </Route>
           <Route path="/login">
             <p>Login page</p>
+            <Login />
           </Route>
           <Route path="/">
             <p>Home page</p>
