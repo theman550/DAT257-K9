ca0cefe9b2a6c21716b9adbd78d76cc8e29989a6
Andreas Wickström Johansson <andreaswjoh@gmail.com>
2020-10-01 12:11:34 +0200

    Add navbar size to theme and use with page wrapper margin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import Navigation from './components/Navigation';
 import Trips from './screens/Trips';
 import Notification from './components/Notification';
 import theme from './themes/base';
 import Account from './screens/Account';
+
+const PageWrapper = styled.div`
+  margin-top: ${(props) => props.theme.size.navbar};
+`;
 
 const App = () => {
   const [notification, setNotification] = useState(null);
@@ -25,17 +29,19 @@ const App = () => {
         <Navigation />
         {notification
           && <Notification msg={notification.msg} color={notification.color} /> }
-        <Switch>
-          <Route path="/account">
-            <Account />
-          </Route>
-          <Route path="/trips">
-            <Trips />
-          </Route>
-          <Route path="/">
-            <p>Welcome to Share-a-ride</p>
-          </Route>
-        </Switch>
+        <PageWrapper>
+          <Switch>
+            <Route path="/account">
+              <Account />
+            </Route>
+            <Route path="/trips">
+              <Trips />
+            </Route>
+            <Route path="/">
+              <p>Welcome to Share-a-ride</p>
+            </Route>
+          </Switch>
+        </PageWrapper>
       </Router>
     </ThemeProvider>
   );
