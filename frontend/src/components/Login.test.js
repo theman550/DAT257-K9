52632746852ad43b798e65ab54a026e7598c62e4
KaramAghi <aghi.karam@gmail.com>
2020-10-09 00:40:34 +0200

    Fix the test

--- frontend/src/components/Login.test.js
+++ frontend/src/components/Login.test.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
 import Login from './Login';
+
+
+const theme = {
+  colors: {
+    primary: '#8064f7',
+    secondary: '#f5ba9c',
+    fill: '#1a1a1a',
+    alternateFill: '#f0ebff',
+    inactive: '#707386',
+  },
+  padding: {
+    section: '1rem 1rem',
+  },
+  size: {
+    navbar: '60px',
+    corner: '0.5rem',
+    button: '0.75rem',
+  },
+  spacing: {
+    subsection: '1rem',
+    section: '2rem',
+  },
+
+};
+
+
 
 describe('Login', () => {
   beforeEach(() => {
     render(
-      <Login />,
+      <ThemeProvider theme={theme}>
+        <Login
+          showNotification={() => ''}
+        />
+      </ThemeProvider>,
     );
   });
 
