a8752c1f833d71bc33eaa8113fea2e6a6a9e6753
Andreas Wickström Johansson <andreaswjoh@gmail.com>
2020-10-11 15:18:07 +0200

    Add mocked trips in its own module

---
+++ frontend/src/mocks/Trips.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    tripID: '0',
+    startLocation: 'Göteborg',
+    destination: 'Malmö',
+    driver: {
+      firstName: 'David',
+      lastName: 'Hernandez',
+    },
+    startTime: new Date(),
+    seatsAvailable: 1,
+  },
+  {
+    tripID: '1',
+    startLocation: 'Göteborg',
+    destination: 'Stockholm',
+    driver: {
+      firstName: 'David',
+      lastName: 'Hernandez',
+    },
+    startTime: new Date(),
+    seatsAvailable: 4,
+  },
+  {
+    tripID: '2',
+    startLocation: 'Göteborg',
+    destination: 'Kinna',
+    driver: {
+      firstName: 'David',
+      lastName: 'Hernandez',
+    },
+    startTime: new Date(),
+    seatsAvailable: 2,
+  },
+  {
+    tripID: '3',
+    startLocation: 'Göteborg',
+    destination: 'Borås',
+    driver: {
+      firstName: 'David',
+      lastName: 'Hernandez',
+    },
+    startTime: new Date(),
+    seatsAvailable: 1,
+  },
+];
