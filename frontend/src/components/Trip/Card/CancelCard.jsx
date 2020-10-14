da1abc7d0d26e914489bb4e305ce527c8f1318af
Andreas Wickström Johansson <andreaswjoh@gmail.com>
2020-10-14 22:43:49 +0200

    Implement cancel booking request to cancel card component

--- frontend/src/components/Trip/Card/CancelCard.jsx
+++ frontend/src/components/Trip/Card/CancelCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
-import styled from 'styled-components';
+import styled, { withTheme } from 'styled-components';
 import PropTypes from 'prop-types';
 import TripCard from './Card';
 import TripModel from '../../../model/Trip';
+import BookingModel from '../../../model/Booking';
+import ThemeShape from '../../../model/ThemeShape';
+import config from '../../../config';
 import { InactiveButton } from '../../UI';
 
 const StyledForm = styled(Form)`
@@ -23,9 +26,22 @@ const StyledForm = styled(Form)`
     }
 `;
 
-const CancelCard = ({ trip }) => {
-  const cancelBooking = (id, seats) => {
+const CancelCard = ({ trip, showNotification, theme }) => {
+  const cancelBooking = async (id, seats) => {
     console.log(`Cancelling booking with id: ${id} for ${seats} seats`);
+
+    const res = await fetch(`${config.api.url}bookings/${id}/`, { method: 'DELETE', mode: 'cors' });
+    const data = await res.json();
+
+    try {
+      if (!res.ok) {
+        throw new Error(data.data || data.message || 'No error message provided');
+      }
+    } catch (error) {
+      return showNotification(error.message, theme.colors.error, 5);
+    }
+
+    return showNotification('Booking deleted', theme.colors.success, 5);
   };
 
   return (
@@ -48,7 +64,13 @@ const CancelCard = ({ trip }) => {
 };
 
 CancelCard.propTypes = {
-  trip: PropTypes.shape(TripModel).isRequired,
+  trip: PropTypes.shape({
+    ...TripModel,
+    // Expect a booking object, that this component can request to cancel
+    booking: PropTypes.shape(BookingModel),
+  }).isRequired,
+  showNotification: PropTypes.func.isRequired,
+  theme: ThemeShape.isRequired,
 };
 
-export default CancelCard;
+export default withTheme(CancelCard);
