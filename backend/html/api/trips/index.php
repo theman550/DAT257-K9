85de7166d473b2fad6909ed8d9503f921a037556
Emil Holmberg <emilholmberg@live.se>
2020-10-15 13:41:51 +0200

    bugfix

--- backend/html/api/trips/index.php
+++ backend/html/api/trips/index.php
@@ -1,8 +1,8 @@
 <?php
 define("ABS_PATH", $_SERVER['DOCUMENT_ROOT']);
 
-//include(ABS_PATH . "/DAT257-K9/backend/html/api.php");
-include(ABS_PATH . "/api.php");
+include(ABS_PATH . "/DAT257-K9/backend/html/api.php");
+//include(ABS_PATH . "/api.php");
 //include(ABS_PATH . "/agilecourse/api.php");
 
 headers();
@@ -16,7 +16,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
 	$filterArray = getTripGETParameters();
-	$response = readFilteredTable($filterArray, "Resa", returnStringQuery($filterArray, "=") . returnStringQuery(Array("startTime"), ">=") . "ORDER BY startTime ASC");
+	if(isset($_GET['startTime']))
+	{
+		$response = readFilteredTable($filterArray, "Resa", returnStringQuery($filterArray, "=") . returnStringQuery(Array("startTime"), ">=") . "ORDER BY startTime ASC");
+	}
+	else if(isset($_GET['price']))
+	{
+		$response = readFilteredTable($filterArray, "Resa", returnStringQuery($filterArray, "=") . returnStringQuery(Array("price"), ">=") . "ORDER BY price ASC");
+	}
 	sendResponseQuery($response);
 }
 
@@ -28,18 +35,10 @@ else if($_SERVER['REQUEST_METHOD'] === 'POST'){
 		{	
 			$response = writeTrip($data); // ändra writetrip så att den tar emot alla parametrar som förut så det inte blir error
 			sendResponseString($response);
-			/*if(isset($data->startLocation) && isset($data->destination) && isset($data->price) && isset($data->startTime) && isset($data->seatsAvailable) && isset($data->description) && isset($data->userID))
-			{
-				$response = writeTrip($data->startLocation, $data->destination, $data->price, $data->startTime, $data->seatsAvailable, $data->description, $data->userID); // ändra writetrip så att den tar emot alla parametrar som förut så det inte blir error
-				sendResponseString($response);
-			}*/
-				//$response = writeTrip($_POST['startLocation'], $_POST['destination'], $_POST['price'], $_POST['startTime'], $_POST['seatsAvailable'], $_POST['description'], $_POST['userID']);
-
-			http_response_code(201);
 		}
 		else
 		{
-			http_response_code(401); // tänker att vi kör 401:a om felaktigt token/mail och 400:a om requesten är felaktig
+			http_response_code(401);
 		}
 	}
 	else
